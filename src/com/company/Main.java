dea2c2e408c10aa1cd58ff2fd6d7c51752f56005
rsoriano1201 <rsoriano1201@gmail.com>
2021-06-12 10:11:39 -0700

    final red audi car at your service

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,15 @@ package com.company;
 
 public class Main {
 
+public static void main(String[] args){
+
     car audi = new car();
+    audi.color = "red";
+    audi.number = "112";
+    audi.door = "3";
+    audi.model = "A3";
 
     
+}
+
 }
